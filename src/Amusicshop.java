2dd4e855444911f0cd545a3e4b12d165bf9bd4e7
staspetrenko <stas.petrenko26@gmail.com>
2016-06-15 14:56:35 +0000

    HW3.3

---
+++ src/Amusicshop.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+/**
+ * Created by Stas on 6/15/2016.
+ */
+public class Amusicshop {
+    private List<Musicalinstrument> musicalinstruments;
+
+    public List<Musicalinstrument> getMusicalinstruments() {
+        return musicalinstruments;
+    }
+
+    public void setMusicalinstruments(List<Musicalinstrument> musicalinstruments) {
+        this.musicalinstruments = musicalinstruments;
+    }
+}
