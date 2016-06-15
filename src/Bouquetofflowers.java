f51f96cff31f33a19a19aec18a36beb4ad7c7889
staspetrenko <stas.petrenko26@gmail.com>
2016-06-15 14:46:02 +0000

    HW3.2

---
+++ src/Bouquetofflowers.java
@@ -0,0 +1,52 @@
+import java.util.List;
+
+/**
+ * Created by Stas on 6/15/2016.
+ */
+public class Bouquetofflowers {
+    private List<Aster> aster;
+    private List<Daisy> daisie;
+    private List<Rose> rose;
+    private List<Rosebush> rosebush;
+    private List<Tulip> tulip;
+
+    public List<Aster> getAster() {
+        return aster;
+    }
+
+    public void setAster(List<Aster> aster) {
+        this.aster = aster;
+    }
+
+    public List<Daisy> getDaisie() {
+        return daisie;
+    }
+
+    public void setDaisie(List<Daisy> daisie) {
+        this.daisie = daisie;
+    }
+
+    public List<Rose> getRose() {
+        return rose;
+    }
+
+    public void setRose(List<Rose> rose) {
+        this.rose = rose;
+    }
+
+    public List<Rosebush> getRosebush() {
+        return rosebush;
+    }
+
+    public void setRosebush(List<Rosebush> rosebush) {
+        this.rosebush = rosebush;
+    }
+
+    public List<Tulip> getTulip() {
+        return tulip;
+    }
+
+    public void setTulip(List<Tulip> tulip) {
+        this.tulip = tulip;
+    }
+}
---
+++ src/Rosebush.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+/**
+ * Created by Stas on 6/15/2016.
+ */
+public class Rosebush extends Flower {
+    List<Rose> rose;
+
+    public List<Rose> getRose() {
+        return rose;
+    }
+
+    public void setRose(List<Rose> rose) {
+        this.rose = rose;
+    }
+}
